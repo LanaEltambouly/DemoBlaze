d522d905f04c092bc2e66e0aaf0324f34b38c02f
Lana Eltambouly <lanaeltambouly@gmail.com>
2025-11-26 00:55:02 +0200

    Eighth Commit

--- src/test/java/Pages/Categories/LaptopsCatPage.java
+++ src/test/java/Pages/Categories/LaptopsCatPage.java
@@ -2,10 +2,13 @@ package Pages.Categories;
 
 import Pages.HomePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
 
 public class LaptopsCatPage extends HomePage{
     WebDriver driver;
-
+    //List<WebElement>
     public LaptopsCatPage(WebDriver driver){
         super(driver);
         this.driver = driver;
